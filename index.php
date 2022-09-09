1f27cb477d37d355b511ff15f7ce876ebf336ee1
Iuri Valbão <iuri.valbao@gmail.com>
2022-09-09 19:10:51 -0300

    Update index.php
    Mudança de cor de H2

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site em PHP</title>
+    <style>
+        h2 {
+            color: azul;
+        }
+    </style>
 </head>
 <body>
     <h1>Testando PHP</h1>
     <?php
-        echo "Olá Mundo!";
+        echo <h2>"Olá Mundo!"</h2>;
     ?>
 </body>
 </html>
