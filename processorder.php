aff30c82cf2ac2e074f631496a56c7ac81c1e9b7
Iuri Valbão <55365008+iuri-valbao@users.noreply.github.com>
2023-08-24 23:18:13 -0300

    correction 001

--- processorder.php
+++ processorder.php
@@ -4,10 +4,22 @@
         <title>Bob's Auto Parts  -  Order Results</title>
     </head>
     <body>
+        <?php
+        //create short variable names
+        $tireqty = $_POST['tireqty'];
+        $oilqty = $_POST['oilqty'];
+        $sparkqty = $_POST['sparkqty'];
+        ?>
         <h1>Bob's Auto Parts</h1>
         <h2>Order Results</h2>
         <?php
-            echo '<p>Order processed.</p>';
+            echo "<p>Order processed at ".date('H:i, jS F Y')."</p>";
+        ?>
+        <?php
+        echo '<p>Your order is as follows:</p>';
+        echo htmlspecialchars($tireqty).' tires<br />';
+        echo htmlspecialchars($oilqty).' bottles of oil<br />';
+        echo htmlspecialchars($sparkqty).' spark plugs<br />';
         ?>
     </body>
 </html>
